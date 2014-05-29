b700c74ddc1c31e767e26c663d1f854c13daf90d
Rachel S. Wang <rachel.wang@rachel-wang-ltm.internal.salesforce.com>
2014-05-29 15:02:04 -0700

    styling

--- example.js
+++ example.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.send('<h1 style="font-family:Menlo; color:#551A8B;">Hello World!</h1>');
 });
 
 var port = Number(process.env.PORT || 5000);
