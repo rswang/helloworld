d59b05bf9fd5cc8493a88d6170e066236a1bcdaf
Rachel S. Wang <rachel.wang@rachel-wang-ltm.internal.salesforce.com>
2014-05-30 13:21:07 -0700

    Jade homepage

--- example.js
+++ example.js
@@ -3,15 +3,29 @@ require("newrelic");
 // example taken from heroku
 var express = require("express");
 var logfmt = require("logfmt");
+var path = require("path");
+
 var app = express();
+
+app.configure(function() {
+	app.set('port', 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('<h1 id="test" style="font-family:Avenir; font-weight: lighter; color:#551A8B;">Hello World!</h1>');
+  // res.send('<h1 id="test" style="font-family:Avenir; font-weight: lighter; color:#551A8B;">Hello World!</h1>');
+	res.render('index', {title: 'Login'});
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
+
+// var port = Number(process.env.PORT || 3000);
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
