0b552da1d45b59d89a940892a44919186c7cfa35
Rachel S. Wang <rachel.wang@rachel-wang-ltm.internal.salesforce.com>
2014-05-30 13:31:37 -0700

    Remove logger

--- example.js
+++ example.js
@@ -2,7 +2,6 @@ require("newrelic");
 
 // example taken from heroku
 var express = require("express");
-var logfmt = require("logfmt");
 var path = require("path");
 
 var app = express();
@@ -11,19 +10,18 @@ app.configure(function() {
 	app.set('port', 3000);
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
+    app.use(express.cookieParser()),
     app.use(express.static(path.join(__dirname, 'public')));
 });
-
-app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('<h1 id="test" style="font-family:Avenir; font-weight: lighter; color:#551A8B;">Hello World!</h1>');
 	// res.render('public/index.jade', {title: 'Login'});
 });
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function () {
+//     console.log("Express server listening on port " + app.get('port'));
+// });
 
 // var port = Number(process.env.PORT || 3000);
 // app.listen(port, function() {
